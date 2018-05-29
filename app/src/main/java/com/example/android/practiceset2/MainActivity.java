7690fc3c98ae8691f02810babd1e8c8dc81c6141
aliadil <aliadilg@Gmail.com>
2018-05-30 03:27:28 +0500

    update

--- app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -10,11 +10,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // PASTE CODE YOU WANT TO TEST HERE
-        int raspberryPrice = 5;
-        display1("1 box: $" + raspberryPrice);
-        raspberryPrice = 10;
-        display2("2 boxes: $" + (raspberryPrice));
-        display3("3 boxes: $" + (raspberryPrice * 3));
+        int weekday = 5;
+        int weekend = 9;
+        int optimalHours = 7 * 8;
+
+        int actualHours = 0;
+        actualHours = (weekday *5) + (weekend * 2);
+        int solution = optimalHours - actualHours;
+        display(solution);
     }
     /**
      * Display methods that allow the text to appear on the screen. Don't worry if you don't know
@@ -22,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
      */
 
 
-   
+
     public void display(String text) {
         TextView t = (TextView) findViewById(R.id.display_text_view);
         t.setText(text);
